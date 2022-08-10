a75055d53010dae8eb1606fb835b806b7150173f
dimkary <dim.karypidis@outlook.com>
2022-08-10 14:24:07 +0200

    Screen toggle ML script implemented

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,79 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Store last pixel address
+@8192
+D=A
+
+@maxpix
+M = D //(8191 total pixels)
+
+(INFINITELOOP)
+	
+	// Read the keyboard input. Store it in register R0
+	@KBD
+	D=M
+	
+	@pressed
+	M=D
+	D=M
+	
+	@i
+	M = 0
+	
+	@LIGHT
+	D;JGT
+	
+	@DARK
+	D;JEQ
+
+(LIGHT)
+	
+	@i
+	D = M
+	
+	@maxpix
+	D = M - D
+	@INFINITELOOP
+	D; JEQ
+	
+	@SCREEN
+	D = A
+	
+	@i
+	D = D + M
+	A = D
+	M = -1
+	
+	@i
+	D = M + 1
+	M = D
+	
+	@LIGHT
+	0;JMP
+	
+(DARK)
+	
+	@i
+	D = M
+	
+	@maxpix
+	D = M - D
+	@INFINITELOOP
+	D; JEQ
+	
+	@SCREEN
+	D = A
+	
+	@i
+	D = D + M
+	A = D
+	M = 0
+	
+	@i
+	D = M + 1
+	M = D
+	
+	@DARK
+	0;JMP
